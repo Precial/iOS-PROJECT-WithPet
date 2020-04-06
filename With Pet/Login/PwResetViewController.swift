379202e022373dcced8b8de9bd406c1f72889c0e
장성구 <ggrty10@gmail.com>
2020-04-06 21:45:15 +0900

    비밀번호 찾기 화면 팝업 형식으로 교체

--- With Pet/Login/PwResetViewController.swift
+++ With Pet/Login/PwResetViewController.swift
@@ -18,8 +18,10 @@ class PwResetViewController: UIViewController {
 
     // 비밀번호를 찾을 입력
     @IBOutlet weak var findID: UITextField!
+    @IBOutlet weak var popupView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        popupView.layer.cornerRadius = 30
     }
     
     // 비밀번호 재설정 버튼
@@ -28,13 +30,16 @@ class PwResetViewController: UIViewController {
        createUserMessage()
     }
     
+    @IBAction func cancleBtn(_ sender: Any) {
+         dismiss(animated: true, completion: nil)
+    }
     
     // 알람창 띄우는 함수
     func createUserMessage(){
            let alert = UIAlertController(title: "", message: "요청하신 이메일로 발송하였습니다.", preferredStyle: .alert)
                alert.addAction(UIAlertAction(title: "확인", style: .default){
                UIAlertAction in
-                self.navigationController?.popViewController(animated: true)
+                self.dismiss(animated: true, completion: nil)
          })
         present(alert, animated: true, completion: nil)
        }
